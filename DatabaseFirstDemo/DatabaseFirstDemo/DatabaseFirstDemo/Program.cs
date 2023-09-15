0fd403f3ac182c886f14bdc7f4cbde9836f91417
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-15 13:01:56 +0530

    Add project files.

---
+++ DatabaseFirstDemo/DatabaseFirstDemo/DatabaseFirstDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DatabaseFirstDemoEntities();
+
+            var post = new Post
+            {
+                Body = "Body",
+                DatePublished = DateTime.Now,
+                Title = "Title",
+                PostId = 1
+            };
+
+            db.Posts.Add(post);
+
+            db.SaveChanges();
+
+        }
+    }
+}
