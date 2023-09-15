b25307dbe356cbdce63a44776a269f856e0f8374
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-15 18:30:51 +0530

    Section-3-Solution - DbFirst Approch.

---
+++ DbFirst/Section-3-Solution - DbFirst/Vidzy/Vidzy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new VidzyDbContext();
+
+            db.AddVideo("Video 1", DateTime.Today, 2, (byte)Classification.Platinum);
+
+            Console.ReadKey();
+        }
+    }
+}
